a0e4a6672ba4fcadbf9f032d608588d26e0e8771
Pedro Gomes <pedrogomes94@gmail.com>
2018-10-12 00:02:03 -0700

    add redis

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+// const redisClient = require('./redis');
+
+const EXPRESS_PORT = process.env.EXPRESS_PORT || 3040;
+
 const app = express();
-const port = 3040;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(EXPRESS_PORT, () =>
+  console.log(`Express app listening on port ${EXPRESS_PORT}!`)
+);
