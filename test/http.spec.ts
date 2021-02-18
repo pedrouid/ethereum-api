b485ff4ac7140f7e2c89cff8eddd02d14df4ae7c
Pedro Gomes <pedrogomes94@gmail.com>
2021-02-18 15:45:46 +0100

    improve tests, change assets and add optimism

--- test/http.spec.ts
+++ test/http.spec.ts
@@ -1,114 +1,103 @@
 import "mocha";
-import { use, expect } from "chai";
+import chai from "chai";
 import chaiHttp from "chai-http";
+
+chai.use(chaiHttp);
+
+import { FastifyInstance } from "fastify";
+
 import { HttpService } from "../src/http";
 
-use(chaiHttp);
+import { testAccountAssets, testAccountBalance } from "./shared";
 
 const TEST_ACCOUNT_ADDRESS = "0xed4de13de52c03f7FC85025B051E5C82Cbe20F2A";
 
 const EXPECTED_ETH_BALANCE = {
   symbol: "ETH",
-  name: "Ethereum",
+  name: "Ether",
   decimals: "18",
   contractAddress: "",
   balance: "0",
 };
 
 describe("HTTP", () => {
+  let app: FastifyInstance;
+  before(() => {
+    const http = new HttpService({ logger: "fatal" });
+    app = http.app;
+  });
   it("GET hello", done => {
-    const { app } = new HttpService({ logger: "fatal" });
     app.inject({ method: "GET", url: "/hello" }, (err, response) => {
-      expect(err).to.be.null;
-      expect(response.statusCode).to.equal(200);
-      expect(response.body).to.equal(`Hello World`);
-      expect(true).to.be.true;
-      app.close(); // THis is not working so we need --exit in mocha
+      chai.expect(err).to.be.null;
+      chai.expect(response.statusCode).to.equal(200);
+      chai.expect(response.body).to.equal(`Hello World`);
+      chai.expect(true).to.be.true;
       done();
     });
   });
+
   it("GET account balance with chainId=1", done => {
-    const { app } = new HttpService({ logger: "fatal" });
-    const address = TEST_ACCOUNT_ADDRESS;
-    const chainId = 1;
-    app.inject(
-      { method: "GET", url: `/account-balance?address=${address}&chainId=${chainId}` },
-      (err, response) => {
-        expect(err).to.be.null;
-        expect(response.statusCode).to.equal(200);
-        expect(response.body).to.equal(
-          JSON.stringify({
-            success: true,
-            result: EXPECTED_ETH_BALANCE,
-          }),
-        );
-        expect(true).to.be.true;
-        app.close(); // THis is not working so we need --exit in mocha
-        done();
-      },
-    );
+    const params = {
+      address: TEST_ACCOUNT_ADDRESS,
+      chainId: 1,
+      expected: EXPECTED_ETH_BALANCE,
+    };
+    testAccountBalance(app, params, done);
   });
   it("GET account balance with chainId=5", done => {
-    const { app } = new HttpService({ logger: "fatal" });
-    const address = TEST_ACCOUNT_ADDRESS;
-    const chainId = 1;
-    app.inject(
-      { method: "GET", url: `/account-balance?address=${address}&chainId=${chainId}` },
-      (err, response) => {
-        expect(err).to.be.null;
-        expect(response.statusCode).to.equal(200);
-        expect(response.body).to.equal(
-          JSON.stringify({
-            success: true,
-            result: EXPECTED_ETH_BALANCE,
-          }),
-        );
-        expect(true).to.be.true;
-        app.close(); // THis is not working so we need --exit in mocha
-        done();
-      },
-    );
+    const params = {
+      address: TEST_ACCOUNT_ADDRESS,
+      chainId: 5,
+      expected: EXPECTED_ETH_BALANCE,
+    };
+    testAccountBalance(app, params, done);
+  });
+  it("GET account balance with chainId=10", done => {
+    const params = {
+      address: TEST_ACCOUNT_ADDRESS,
+      chainId: 10,
+      expected: EXPECTED_ETH_BALANCE,
+    };
+    testAccountBalance(app, params, done);
+  });
+  it("GET account balance with chainId=420", done => {
+    const params = {
+      address: TEST_ACCOUNT_ADDRESS,
+      chainId: 420,
+      expected: EXPECTED_ETH_BALANCE,
+    };
+    testAccountBalance(app, params, done);
   });
   it("GET account assets with chainId=1", done => {
-    const { app } = new HttpService({ logger: "fatal" });
-    const address = TEST_ACCOUNT_ADDRESS;
-    const chainId = 1;
-    app.inject(
-      { method: "GET", url: `/account-assets?address=${address}&chainId=${chainId}` },
-      (err, response) => {
-        expect(err).to.be.null;
-        expect(response.statusCode).to.equal(200);
-        expect(response.body).to.equal(
-          JSON.stringify({
-            success: true,
-            result: [EXPECTED_ETH_BALANCE],
-          }),
-        );
-        expect(true).to.be.true;
-        app.close(); // THis is not working so we need --exit in mocha
-        done();
-      },
-    );
+    const params = {
+      address: TEST_ACCOUNT_ADDRESS,
+      chainId: 1,
+      expected: [EXPECTED_ETH_BALANCE],
+    };
+    testAccountAssets(app, params, done);
   });
   it("GET account assets with chainId=5", done => {
-    const { app } = new HttpService({ logger: "fatal" });
-    const address = TEST_ACCOUNT_ADDRESS;
-    const chainId = 1;
-    app.inject(
-      { method: "GET", url: `/account-assets?address=${address}&chainId=${chainId}` },
-      (err, response) => {
-        expect(err).to.be.null;
-        expect(response.statusCode).to.equal(200);
-        expect(response.body).to.equal(
-          JSON.stringify({
-            success: true,
-            result: [EXPECTED_ETH_BALANCE],
-          }),
-        );
-        expect(true).to.be.true;
-        app.close(); // THis is not working so we need --exit in mocha
-        done();
-      },
-    );
+    const params = {
+      address: TEST_ACCOUNT_ADDRESS,
+      chainId: 5,
+      expected: [EXPECTED_ETH_BALANCE],
+    };
+    testAccountAssets(app, params, done);
+  });
+  it("GET account assets with chainId=10", done => {
+    const params = {
+      address: TEST_ACCOUNT_ADDRESS,
+      chainId: 10,
+      expected: [EXPECTED_ETH_BALANCE],
+    };
+    testAccountAssets(app, params, done);
+  });
+  it("GET account assets with chainId=420", done => {
+    const params = {
+      address: TEST_ACCOUNT_ADDRESS,
+      chainId: 420,
+      expected: [EXPECTED_ETH_BALANCE],
+    };
+    testAccountAssets(app, params, done);
   });
 });
