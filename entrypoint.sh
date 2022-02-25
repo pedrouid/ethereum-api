f9141c52904926ad4371c78b6f509816b25a8ccd
ss75710541 <75710541@qq.com>
2022-02-25 11:45:55 +0800

    fix: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-SOLAR_NETWORK_URL=${SOLAR_NETWORK_URL:-https://test-sct.netwarps.com}
 
 # replace the frented params
-sed -i "s|https://test-sct.netwarps.com/|${SOLAR_NETWORK_URL}|g" /dist/chains.js
+
+if [ ! -z "${SOLAR_NETWORK_URL}" ];then
+    sed -i "s|https://test-sct.netwarps.com/|${SOLAR_NETWORK_URL}|g" /dist/chains.js
+fi
 
 # run 
 exec node /dist/index.js
