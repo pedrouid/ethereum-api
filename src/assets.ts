bc558cb8008a7c67d43a04e4431134e274106af7
ss75710541 <75710541@qq.com>
2022-03-01 16:22:06 +0800

    feat: 只保留Solar Network

--- src/assets.ts
+++ src/assets.ts
@@ -7,41 +7,6 @@ export const ETH: IAssetData = {
   contractAddress: "",
 };
 
-export const RSK: IAssetData = {
-  symbol: "RSK",
-  name: "RSK",
-  decimals: "18",
-  contractAddress: "",
-};
-
-export const POA: IAssetData = {
-  symbol: "POA",
-  name: "POA",
-  decimals: "18",
-  contractAddress: "",
-};
-
-export const xDAI: IAssetData = {
-  symbol: "xDAI",
-  name: "xDAI",
-  decimals: "18",
-  contractAddress: "",
-};
-
-export const MATIC: IAssetData = {
-  symbol: "MATIC",
-  name: "Matic",
-  decimals: "18",
-  contractAddress: "",
-};
-
-export const CLO: IAssetData = {
-  symbol: "CLO",
-  name: "CLO",
-  decimals: "18",
-  contractAddress: "",
-};
-
 export const SOLAR: IAssetData = {
   symbol: "SOLAR",
   name: "Solar",
