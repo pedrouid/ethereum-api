893f7f37cbcc884dc2b9839bd9a12087cb7436ed
ss75710541 <75710541@qq.com>
2022-03-01 18:35:44 +0800

    feat: add env SOLAR_BLOCKSCOUT_URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,9 @@ if [ ! -z "${SOLAR_NETWORK_URL}" ];then
     sed -i "s|https://test-sct.netwarps.com/|${SOLAR_NETWORK_URL}|g" dist/chains.js
 fi
 
+if [ ! -z "${SOLAR_BLOCKSCOUT_URL}" ];then
+    sed -i "s|https://blockscout.com/|${SOLAR_BLOCKSCOUT_URL}|g" dist/blockscout.js
+fi
+
 # run 
 exec node dist/index.js
