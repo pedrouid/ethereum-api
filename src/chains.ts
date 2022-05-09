a0e23c0c298fff1e1cf554ed3407e7023806e633
ss75710541 <75710541@qq.com>
2022-05-09 17:46:16 +0800

    src/chains.ts add 65535 链信息

--- src/chains.ts
+++ src/chains.ts
@@ -12,6 +12,16 @@ const supportedChains: IChainData[] = [
     rpc_url: "https://test-sct.netwarps.com/",
     native_currency: ETH,
   },
+  {
+    name: "Solar Network UAT",
+    short_name: "solar",
+    chain: "Solar",
+    network: "UAT",
+    chain_id: 65535,
+    network_id: 65535,
+    rpc_url: "https://sct.netwarps.com/",
+    native_currency: ETH,
+  },
 ];
 
 export default supportedChains;
